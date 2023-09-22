8aa188f8c3a32fb334963bf21d5875b32dcb31ed
Jaime Pineda <jpin730@pm.me>
2023-09-22 16:55:20 -0600

    chore: :wrench: configure eslint and prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className={inter.className}>
-      <h1 className="text-lg">Next Open Jira</h1>
-    </main>
+    <>
+      <Head>
+        <title>Next Open Jira</title>
+      </Head>
+      <main className={inter.className}>
+        <h1 className="text-lg">Next Open Jira</h1>
+      </main>
+    </>
   );
 }
