a03c4840f8f8251c3945b579763d6fb0eb0e11f6
Jaime Pineda <jpin730@pm.me>
2023-10-05 15:57:00 -0600

    refactor: :recycle: refactor middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,21 @@
 import { type NextRequest, NextResponse } from 'next/server'
 
 export function middleware(req: NextRequest): NextResponse {
-  if (req.nextUrl.pathname.startsWith('/api/entries/')) {
-    const id = req.nextUrl.pathname.replace('/api/entries/', '')
-    const checkMongoIDRegExp = /^[0-9a-fA-F]{24}$/
-    if (!checkMongoIDRegExp.test(id)) {
-      const url = req.nextUrl.clone()
-      url.pathname = '/api/bad-request'
-      url.search = `?message=${id} is not a valid Mongo Id`
-      return NextResponse.rewrite(url)
-    }
+  const id = req.nextUrl.pathname.split('/').at(-1) ?? ''
+  const checkMongoIDRegExp = /^[0-9a-fA-F]{24}$/
+  if (!checkMongoIDRegExp.test(id)) {
+    const message = `${id} is not a valid Mongo Id`
+    return NextResponse.json(
+      {
+        message,
+      },
+      { status: 400 },
+    )
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: '/api/entries/:path*',
+  matcher: '/api/entries/:id+',
 }
