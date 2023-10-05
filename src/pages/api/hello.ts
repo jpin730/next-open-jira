bfaa715f36a351e2d33190db568d9e8dd3f9acba
Jaime Pineda <jpin730@pm.me>
2023-10-05 16:06:55 -0600

    refactor: :recycle: refactor hello endpoint

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 interface Data {
-  name: string
+  message: string
+  method?: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ): void {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ message: 'Hello world!', method: req.method })
 }
